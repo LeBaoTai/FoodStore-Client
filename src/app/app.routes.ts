dc7b1e26193273c12969b662334e8a5a4638ab45
LBT <fnight644@gmail.com>
2024-06-22 18:18:57 +0700

    update header and add side bar to home view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Food Store' },
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'account', component: AccountComponent, title: 'My Account' },
+  { path: 'cart', component: CartComponent, title: 'Cart'}
 ];
