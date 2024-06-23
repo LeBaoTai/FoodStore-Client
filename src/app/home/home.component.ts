fdda285fc5bbdc00053db19a441ecdd57d440dd2
LBT <fnight644@gmail.com>
2024-06-23 20:29:52 +0700

    add product component to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SideBarComponent } from '../component/side-bar/side-bar.component';
+import { ProductComponent } from '../component/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, SideBarComponent],
+  imports: [RouterLink, RouterOutlet, SideBarComponent, ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
