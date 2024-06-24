955b4dacf96b3cca7487a0278ad1c5e575eece68
LBT <fnight644@gmail.com>
2024-06-24 19:23:58 +0700

    Use Tile Component Instead of Fixed Div

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from '../component/side-bar/side-bar.component';
+import { CommonModule } from '@angular/common';
+import { Tile } from '../types';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent, CommonModule],
   templateUrl: './account.component.html',
-  styleUrl: './account.component.scss'
+  styleUrl: './account.component.scss',
 })
 export class AccountComponent {
+  accountTitle: string = "Account";
+  accountTiles: Tile[] = [
+    {
+      image: 'assets/account/user.png',
+      title: 'My Infomation'
+    },
 
+    {
+      image: 'assets/account/product.png',
+      title: 'My Products'
+    },
+
+    {
+      image: 'assets/account/order.png',
+      title: 'Manage Order'
+    },
+
+    {
+      image: 'assets/account/history.png',
+      title: 'History'
+    },
+
+    {
+      image: 'assets/account/logout.png',
+      title: 'Logout'
+    },
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,46 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SideBarComponent } from '../component/side-bar/side-bar.component';
 import { ProductComponent } from '../component/product/product.component';
+import { Tile } from '../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, RouterOutlet, SideBarComponent, ProductComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  foodTitle: string = 'Category';
+  foodTiles: Tile[] = [
+    {
+      image: 'assets/category/vegetables.png',
+      title: 'Vegetables',
+    },
 
+    {
+      image: 'assets/category/beef.png',
+      title: 'Beef',
+    },
+
+    {
+      image: 'assets/category/chicken.png',
+      title: 'Chicken',
+    },
+
+    {
+      image: 'assets/category/crab.png',
+      title: 'Crab',
+    },
+
+    {
+      image: 'assets/category/fish.png',
+      title: 'Fish',
+    },
+
+    {
+      image: 'assets/category/pork.png',
+      title: 'Pork',
+    },
+  ];
 }
