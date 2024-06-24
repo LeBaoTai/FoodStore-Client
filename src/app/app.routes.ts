b1c124cdefa4df86769657709723e45101e28f46
LBT <fnight644@gmail.com>
2024-06-24 20:00:51 +0700

    update link route among component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './account/account.component';
 import { CartComponent } from './cart/cart.component';
+import { InfoComponent } from './component/account/info/info.component';
+import { MyProductsComponent } from './component/account/my-products/my-products.component';
+import { ManageOrdersComponent } from './component/account/manage-orders/manage-orders.component';
+import { HistoryComponent } from './component/account/history/history.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Food Store' },
   { path: 'account', component: AccountComponent, title: 'My Account' },
-  { path: 'cart', component: CartComponent, title: 'Cart'}
+  { path: 'cart', component: CartComponent, title: 'Cart'},
+
+  // account path
+  { path: 'account/info', component: InfoComponent, title: 'My Information' },
+  { path: 'account/my-products', component: MyProductsComponent, title: 'My Products' },
+  { path: 'account/manager-orders', component: ManageOrdersComponent, title: 'Orders' },
+  { path: 'account/history', component: HistoryComponent, title: 'History' },
 ];
