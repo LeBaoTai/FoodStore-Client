b8204598c35a1db25d3eeed9560d80fd8f4a723f
LBT <fnight644@gmail.com>
2024-06-24 20:00:25 +0700

    add some account components to account view

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,40 +2,46 @@ import { Component } from '@angular/core';
 import { SideBarComponent } from '../component/side-bar/side-bar.component';
 import { CommonModule } from '@angular/common';
 import { Tile } from '../types';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [SideBarComponent, CommonModule],
+  imports: [SideBarComponent, CommonModule, RouterOutlet, RouterLink],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss',
 })
 export class AccountComponent {
-  accountTitle: string = "Account";
+  accountTitle: string = 'Account';
   accountTiles: Tile[] = [
     {
       image: 'assets/account/user.png',
-      title: 'My Infomation'
+      title: 'My Infomation',
+      url: '/account/info',
     },
 
     {
       image: 'assets/account/product.png',
-      title: 'My Products'
+      title: 'My Products',
+      url: '/account/my-products',
     },
 
     {
       image: 'assets/account/order.png',
-      title: 'Manage Order'
+      title: 'Manage Orders',
+      url: '/manage-orders',
     },
 
     {
       image: 'assets/account/history.png',
-      title: 'History'
+      title: 'History',
+      url: '/account/history',
     },
 
     {
       image: 'assets/account/logout.png',
-      title: 'Logout'
+      title: 'Logout',
+      url: '/account/logout',
     },
   ];
 }
