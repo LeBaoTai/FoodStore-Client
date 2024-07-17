ede7ee3282032bc7bea0bdab1a38c235f89bdf65
FiaT <fnight644@gmail.com>
2024-07-17 22:43:05 +0700

    update account button has 2 two options, information, and sign in or register

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Output, Renderer2 } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RegisterComponent } from '../component/auth/register/register.component';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RegisterComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-
 export class HeaderComponent {
+  isUserSignedIn!: boolean;
 
+  constructor(private authService: AuthService, private render: Renderer2) {
+    this.authService.isUserSignedIn$.subscribe((status) => {
+      this.isUserSignedIn = status;
+    });
+  }
+
+  showInfoPopup(element: HTMLDivElement) {
+    if (!this.isUserSignedIn) {
+      this.render.removeClass(element, 'turn-on');
+    }
+  }
+
+  showRegiter() {
+    if (!this.isUserSignedIn) {
+      this.authService.setShowRegister(true);
+    }
+  }
 }
