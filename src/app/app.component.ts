bda32f0f1da9fd4ae6e216802074a6195c7d367d
FiaT <fnight644@gmail.com>
2024-07-18 23:31:39 +0700

    feat: update UI & logic (#10)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 import { RegisterComponent } from './component/auth/register/register.component';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -26,5 +27,18 @@ import { RegisterComponent } from './component/auth/register/register.component'
   styleUrl: './app.component.scss',
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    if (this.isLoggedIn) {
+      console.log('User is logged in');
+      this.authService.setSignedIn(true);
+    } else {
+      console.log('User is not logged in');
+    }
+  }
 }
